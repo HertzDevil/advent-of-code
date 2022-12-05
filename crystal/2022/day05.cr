56dd3e80a641337b83339d25185f9f594bc10831
Quinton Miller <nicetas.c@gmail.com>
2022-12-05 23:25:21 +0800

    monkeypatch

--- crystal/2022/day05.cr
+++ crystal/2022/day05.cr
@@ -1,5 +1,16 @@
 require "../support"
 
+module Indexable(T)
+  def [](range : Steppable::StepIterator)
+    raise ArgumentError.new "step must be positive: #{range.@step}" unless range.@step > 0
+    step1_range = Range.new(range.@current, range.@limit, range.@exclusive)
+    index, count = Indexable.range_to_index_and_count(step1_range, size) || raise IndexError.new
+    Array.new((count - 1) // range.@step + 1) do |i|
+      unsafe_fetch(index + i * range.@step)
+    end
+  end
+end
+
 solve do
   test <<-INPUT, "CMZ"
         [D]\x20\x20\x20\x20
@@ -17,12 +28,9 @@ solve do
     stacks, _, instrs = input.partition(/\n[\d ]*\n\n/)
 
     stacks = stacks.lines
-      .map(&.chars)
+      .map(&.chars[(1..).step(4)])
       .transpose
-      .each_with_index
-      .select { |_, i| i % 4 == 1 }
-      .map(&.first.skip_while(&.== ' ').reverse!)
-      .to_a
+      .map(&.skip_while(&.== ' ').reverse!)
 
     instrs.each_line do |line|
       count, from, to = line.scan(/\d+/).map(&.[0].to_i)
@@ -50,12 +58,9 @@ solve do
     stacks, _, instrs = input.partition(/\n[\d ]*\n\n/)
 
     stacks = stacks.lines
-      .map(&.chars)
+      .map(&.chars[(1..).step(4)])
       .transpose
-      .each_with_index
-      .select { |_, i| i % 4 == 1 }
-      .map(&.first.skip_while(&.== ' ').reverse!)
-      .to_a
+      .map(&.skip_while(&.== ' ').reverse!)
 
     instrs.each_line do |line|
       count, from, to = line.scan(/\d+/).map(&.[0].to_i)
