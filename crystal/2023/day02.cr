6f92ee12475d3080598d1307c01e4ece8b7b7c26
Quinton Miller <nicetas.c@gmail.com>
2023-12-02 13:15:18 +0800

    cleanup

--- crystal/2023/day02.cr
+++ crystal/2023/day02.cr
@@ -1,5 +1,7 @@
 require "../support"
 
+LIMIT = {"red" => 12, "green" => 13, "blue" => 14}
+
 solve do
   test <<-INPUT, 8
     Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
@@ -13,23 +15,8 @@ solve do
     .sum do |line|
       game, _, records = line.partition(": ")
       id = game.lchop("Game ").to_i
-      records = records.split("; ").map(&.split(", "))
-      next 0 unless records.all? do |cubes|
-                      cubes.all? do |cube|
-                        count, _, color = cube.partition(' ')
-                        case color
-                        when "red"
-                          count.to_i <= 12
-                        when "green"
-                          count.to_i <= 13
-                        when "blue"
-                          count.to_i <= 14
-                        else
-                          true
-                        end
-                      end
-                    end
-      id.to_i
+      records = records.split("; ").map(&.split(", ").map(&.partition(' ')))
+      records.all?(&.all? { |count, _, color| count.to_i <= LIMIT[color] }) ? id.to_i : 0
     end
 end
 
@@ -47,9 +34,8 @@ solve do
       game, _, records = line.partition(": ")
       id = game.lchop("Game ").to_i
       records = records.split("; ").map(&.split(", ").map(&.partition(' ')))
-      r = records.max_of { |v| v.find(&.[2].== "red").try(&.[0].to_i) || 0 }
-      g = records.max_of { |v| v.find(&.[2].== "green").try(&.[0].to_i) || 0 }
-      b = records.max_of { |v| v.find(&.[2].== "blue").try(&.[0].to_i) || 0 }
-      r*g*b
+      {"red", "green", "blue"}.product do |color|
+        records.max_of { |v| v.find(&.[2].== color).try(&.[0].to_i) || 0 }
+      end
     end
 end
